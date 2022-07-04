a788e420dccfaf4b8511b7093ea9fe6c2e8c2643
Yaroslavna2007 <108128055+Yaroslavna2007@users.noreply.github.com>
2022-07-04 21:02:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-g = int(input('введите натуральное число'))
-v = int(input('введите натуральное число'))
-n = int(input('введите натуральное число'))
-if n==v==g:
-    print(3)
-elif n!=v!=g:
-    print(0)
-elif g==n and g!=v and n!=v:
-    print(2)
-elif g==v and g!=n and v!=n:
-    print(2)
-elif v==n and v!=g and n!=g:
-    print(2)
+x1 = int(input('введите номер столбца для первой клетки'))
+x2 = int(input('введите номер строки для первой клетки'))
+y1 = int(input('введите номер столбца для второй клетки'))
+y2 = int(input('введите номер строки для второй клетки'))
+if x1==y1 or x2==y2:
+    print('yes')
+else:
+    print ('no')
